4c2aba1a56ff705d5cad280d3b87b03cc4a3b0fb
Adam Chambers <adammchambers@gmail.com>
2017-03-05 11:45:40 -0500

    Move note logic to client and send MIDI array over sockets

--- index.js
+++ index.js
@@ -16,13 +16,6 @@ const io = require('socket.io').listen(app)
 const midiOutput = new midi.output()
 midiOutput.openVirtualPort('MIDI Stream Output')
 
-const notes = [
-  [144,60,100],
-  [144,62,100],
-  [144,64,100],
-  [144,65,100]
-]
-
 // on socket connection
 io.on('connection', socket => {
   // send welcome message with unique ID
@@ -31,14 +24,8 @@ io.on('connection', socket => {
   // on 'midi' socket message send MIDI message to virtual output
   socket.on('midi', data => {
     console.log('trigger', data)
-
-    // if full MIDI message available then send
     if (data.msg) {
       midiOutput.sendMessage([data.msg['0'], data.msg['1'], data.msg['2']])
-
-    // else pick a note to send
-    } else if (data.note) {
-      midiOutput.sendMessage(notes[Number(data.note) - 1])
     }
   })
 })

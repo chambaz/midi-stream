2e53414283e7065057f690b4d1a345afde30a052
Adam Chambers <adammchambers@gmail.com>
2017-03-05 11:20:35 -0500

    4 large buttons each sending a number over socket which sends different midi message to virtual port

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const io = require('socket.io').listen(app)
 const midiOutput = new midi.output()
 midiOutput.openVirtualPort('MIDI Stream Output')
 
+const notes = [
+  [144,60,100],
+  [144,62,100],
+  [144,64,100],
+  [144,65,100]
+]
+
 // on socket connection
 io.on('connection', socket => {
   // send welcome message with unique ID
@@ -24,7 +31,7 @@ io.on('connection', socket => {
   // on 'midi' socket message send MIDI message to virtual output
   socket.on('midi', data => {
     console.log('trigger', data)
-    midiOutput.sendMessage([144,48,100])
+    midiOutput.sendMessage(notes[Number(data.note) - 1])
   })
 })
 
